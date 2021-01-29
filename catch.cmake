a8f1c56e8a1cc07b698f473710b0af80f1bc9abc
ruhtra <ruhtra.mar@gmail.com>
2021-01-29 18:35:06 +0100

    remove catch_thread

--- catch.cmake
+++ catch.cmake
@@ -2,7 +2,4 @@
 add_library(Catch_static STATIC src/catch.cpp )
 target_include_directories(Catch_static PUBLIC inc)
 
-add_library(Catch_thread_static STATIC src/catch_thread.cpp )
-target_include_directories(Catch_thread_static PUBLIC inc)
-
-install(TARGETS Catch_static Catch_thread_static)
+install(TARGETS Catch_static)
