a83381842129aec2a6f580e2cf695ee0ce1857b2
ruhtra <ruhtra.mar@gmail.com>
2021-01-31 18:43:20 +0100

    clean 7

--- catch.cmake
+++ catch.cmake
@@ -1,5 +1,5 @@
 
 add_library(Catch_static STATIC src/catch.cpp )
 target_include_directories(Catch_static PUBLIC inc)
-
+set_property(TARGET Catch_static PROPERTY POSITION_INDEPENDENT_CODE ON)
 install(TARGETS Catch_static)
